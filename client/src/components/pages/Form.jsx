b704afae310a308e8b03093e5c8abc008bc848fc
agudlc <87042363+agudlc@users.noreply.github.com>
2021-12-07 16:26:20 -0300

    added a lot of verification in the form

--- client/src/components/pages/Form.jsx
+++ client/src/components/pages/Form.jsx
@@ -7,14 +7,28 @@ import styles from "../modules/Form.module.css"
 
 function validate(breed) {
     let errors= {};
+    const nums = new RegExp("^\s*?[0-9]{1,2}\s*$");
+    const numsHeight = new RegExp("^\s*?[0-9]{1,3}\s*$")
     // for (let keys in breed) {
     //     if( breed[`${keys}`] === "" || breed[`${keys}`] === []) {
     //         errors.name = "Some inputs are empty"
     //     }
     // }
     if (!breed.name || !breed.weightMax || !breed.weightMin || !breed.heightMin || !breed.heightMax || 
-        !breed.lifespan) {
+        !breed.lifespan ) {
         errors.name = "Some inputs are empty"
+    }
+    if (!nums.test(breed.weightMax) || !nums.test(breed.weightMin) || !nums.test(breed.lifespan)) {
+        errors.numbers = "Weight and lifespan has to be possible numbers in each field"
+    }
+    if (!numsHeight.test(breed.heightMax) || !numsHeight.test(breed.heightMin)) {
+        errors.numbers2 = "Height has to be a possible number"
+    }
+    if (breed.weightMin > breed.weightMax) {
+        errors.weight = "The min has to be lower to max"
+    }
+    if (breed.heightMin > breed.heightMax) {
+        errors.height = "The min has to be lower to max"
     }
     return errors;
 }
@@ -89,37 +103,63 @@ export default function Form () {
     }, [dispatch]);
 
     return (
-        <div>
+        <div className={styles.divContainer}> 
+            <div>
                 <Link to="/home">
-                <button>GO BACK</button>
+                    <button className={styles.button}>GO BACK</button>
                 </Link>
-            <form onSubmit={handleSubmit}>
-                <label>name</label>
+            </div>
+            <form className={styles.form} onSubmit={handleSubmit}>
+                <label>Name</label>
                 <input name="name" value={breed.name} onChange={handleChangue}></input>
-                <label>heightMin</label>
+                <label>Height Min.</label>
                 <input name="heightMin" value={breed.heightMin} onChange={handleChangue}></input>
-                <label>heightMax</label>
+                <label>Height Max.</label>
                 <input name="heightMax" value={breed.heightMax} onChange={handleChangue}></input>
-                <label>weightMin</label>
+                {errors.height && 
+                <div>
+                    <span>{errors.height}</span>
+                </div>}
+                <label>Weight Min.</label>
                 <input name="weightMin" value={breed.weightMin} onChange={handleChangue}></input>
-                <label>weightMax</label>
+                <label>Weight Max.</label>
                 <input name="weightMax" value={breed.weightMax} onChange={handleChangue}></input>
-                <label>lifespan</label>
+                {errors.weight && 
+                <div>
+                    <span>{errors.weight}</span>
+                </div>}
+                <label>Lifespan</label>
                 <input name="lifespan" value={breed.lifespan} onChange={handleChangue}></input>
                 <select name="temperament" onChange={handleSelect}>{stateTemperament.map((temperaments) =>
                    <option key={temperaments.id} value={temperaments.name}>{temperaments.name}</option>
                 )}
                 </select>
-                {errors.name && (
-                    <p>{errors.name}</p>
-                )}
-                {!errors.name && breed.temperament.length && <button type="submit">CREAR</button>}
+                {!errors.name && breed.temperament.length ? 
+                <div>
+                    <button type="submit">CREAR</button>
+                </div> :
+                <div>
+                    <span>{errors.name}</span>
+                </div>}
+                {errors.numbers && 
+                <div>
+                    <span>{errors.numbers}</span>
+                </div>}
+                {errors.numbers2 && 
+                <div>
+                    <span>{errors.numbers2}</span>
+                </div>}
+                {breed.temperament.map((el) =>
+                    <div>
+                        <p>{el}</p>
+                        <button type="button" onClick={() => handleDelete(el)}>X</button>
+                    </div>)}
             </form>
-            {breed.temperament.map((el) =>
-            <div>
-                <p>{el}</p>
-                <button onClick={() => handleDelete(el)}>X</button>
-            </div>)}
         </div>
     )
 }
+
+
+// {errors.name && (
+//     <p>{errors.name}</p>
+// )}
